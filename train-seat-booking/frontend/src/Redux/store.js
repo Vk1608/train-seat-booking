f2efbdbb0d047275a93c20ce57c2276d6630689e
Vikram <vk1608rocks@gmail.com>
2024-09-29 19:48:23 +0530

    Delete train-seat-booking directory

--- train-seat-booking/frontend/src/Redux/store.js
+++
@@ -1,9 +0,0 @@
-import thunk from "redux-thunk";
-const { legacy_createStore, applyMiddleware, compose } = require("redux");
-const { reducer } = require("./reducer");
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store=legacy_createStore(reducer,composeEnhancers(applyMiddleware(thunk)));
-
-export {store};
